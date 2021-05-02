0aa055ddccdb28ab81a203d2bd7c6e8a615ce07d
Oleksii Khokhlov <xoxlov87@gmail.com>
2021-05-02 19:36:32 +0300

    Added carousels

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(function(){
-
+$(".intro-right-arrow").click(function () {
+    topSectionSlider.trigger('owl.next');
 });
+$(".intro-left-arrow").click(function () {
+    topSectionSlider.trigger('owl.prev');
+});
+
+let topSectionSlider = $("#top-slider").owlCarousel({
+    autoPlay: 5000,
+    items: 1,
+    itemsDesktop: [1024, 1],
+    itemsDesktopSmall: [768, 1],
+    itemsTablet: [480, 1],
+    itemsMobile: 1
+});
+
+
